eee67269f853c5145645aaf6781a7a6b5bcd1a05
Matt T <code.matthew.thompson@gmail.com>
2016-08-11 16:23:01 -0400

    adds example model,controller,service to show api use

--- client/src/app/components/index-page/index.component.ts
+++ client/src/app/components/index-page/index.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { ThingService } from '../../services/thing.service'
+import { HTTP_PROVIDERS } from '@angular/http'
 
 @Component({
   selector: `index-page`,
+  providers: [ThingService,HTTP_PROVIDERS],
   template: `
     I am a IndexPageComponent, hi ^^
+    <hr/>
+    --
+    Things:
+    <ul>
+      <li *ngFor="let thing of things">
+        {{thing.name}} -- {{thing.value}}
+      </li>
+    </ul>
   `
 })
 
-export class IndexPageComponent {
-
+export class IndexPageComponent implements OnInit {
+  things: any;
+  constructor(private _thingservice: ThingService){
+  }
+  ngOnInit(){
+    this._thingservice.getThings()
+      .subscribe(data => this.things = data,
+                error => console.log(error))
+  }
 }
---
+++ client/src/app/services/thing.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers, RequestOptions} from '@angular/http'
+
+
+@Injectable()
+export class ThingService {
+  constructor(private _http: Http){}
+
+  getThings() {
+    return this._http.get("http://localhost:3000/api/v1/things")
+      .map(res => res.json(),
+          error => console.log(error))
+  }
+}
