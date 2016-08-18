b64f75116a407bda7a9c779f934bb513c5607e95
Matt T <code.matthew.thompson@gmail.com>
2016-08-18 16:01:45 -0400

    adds some navigation between examples

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
+import { AuthService } from './services/auth.service'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   template: `
     <h2>{{title}}</h2>
+    <button 
+      type="button" 
+      (click)="_authService.logOut()"
+      *ngIf="_authService.isLoggedIn()">
+        LogOut
+    </button>
+    <button 
+      type="button" 
+      (click)="_router.navigate(['signup'])"
+      *ngIf="!_authService.isLoggedIn()">
+        SignUp
+    </button>
+    <button 
+      type="button" 
+      (click)="_router.navigate(['login'])"
+      *ngIf="!_authService.isLoggedIn()">
+        SignIn
+    </button>
     <router-outlet></router-outlet>
   `,
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
+  constructor(private _router:Router,private _authService:AuthService){
+
+  }
   title = 'app works!';
 }
