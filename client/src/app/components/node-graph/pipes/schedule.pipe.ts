7cf3270d82271f4f6dca9dba1d61e0d9d4bd782c
Matt T <code.matthew.thompson@gmail.com>
2016-09-09 09:56:41 -0400

    fixes bug where some stations would only list one of their directons's prediction

--- client/src/app/components/node-graph/pipes/schedule.pipe.ts
+++ client/src/app/components/node-graph/pipes/schedule.pipe.ts
@@ -6,31 +6,29 @@ import { Pipe, PipeTransform, Injectable } from '@angular/core'
 
 @Injectable()
 export class SchedulePipe implements PipeTransform {
-  transform(stops,schedules) {
+  transform(stops, schedules) {
     var arr = []
     var copy = JSON.parse(JSON.stringify(schedules));
     var arr = []
-    for(let stop in stops){
+    for (let stop in stops) {
       var i = 0
-      for(let direction in copy[stops[stop].route_id]){
+      for (let direction in copy[stops[stop].route_id]) {
         arr.push({
-          schedule: copy[stops[stop].route_id][direction].filter(sch => sch.stop_id == stops[stop].id ),
+          schedule: copy[stops[stop].route_id][direction].filter(sch => sch.stop_id == stops[stop].id),
           route: stops[stop].route_id,
           direction: Object.keys(copy[stops[stop].route_id])[i]
         })
         i++
       }
     }
-    var new_arr = []
-    arr = arr.filter(obj => { 
-      new_arr.push(obj)
-      if(new_arr.filter( o => o.route == obj.route && o.direction == obj.direction).length == 1 &&
-      obj.schedule.length > 0){
-        return true
-      }else{
-        return false
-      }
+    arr = arr.filter(obj => obj.schedule.length > 0)
+    return this.uniq(arr,"direction","route")
+
+  }
+  uniq(a, param, param2) {
+    return a.filter(function (item, pos, array) {
+      return array.map(function (mapItem) { return mapItem[param]; }).indexOf(item[param]) === pos ||
+        array.map(function (mapItem) { return mapItem[param2]; }).indexOf(item[param2]) === pos
     })
-    return arr
   }
 }
