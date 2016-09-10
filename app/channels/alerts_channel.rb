c34450e38d9931edb469076539b728462819893a
Matt T <code.matthew.thompson@gmail.com>
2016-09-10 18:01:40 -0400

    attempting to fix the situation where a person is first to visit site after heroku reboot

--- app/channels/alerts_channel.rb
+++ app/channels/alerts_channel.rb
@@ -2,11 +2,20 @@ class AlertsChannel < ApplicationCable::Channel
   def subscribed
     stream_from 'alerts'
     ab_object = ApplicationController::ALERTS
-    ActionCable.server.broadcast('alerts', encode(ab_object.alerts.to_json))
+    first_send(ab_object)
   end
 
   def encode(str) 
     str = str.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
     str = str.force_encoding(Encoding::UTF_8)
+  end
+
+  def first_send(ab_object)
+    if(ab_object.alerts == {})
+      first_send(ab_object)
+      sleep 2
+    else
+      ActionCable.server.broadcast('alerts', encode(ab_object.alerts.to_json))
+    end
   end
 end
